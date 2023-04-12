5d31b326204f2d9e98e991044b9fb7aa296340de
Seosamh Cahill <seo.cahill@gmail.com>
2023-04-12 15:17:31 +0200

    feat: implement reverse button

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,10 @@
 // This is where it all goes :)
+
+function swapValues() {
+  var from = document.getElementById("from");
+  var to = document.getElementById("to");
+
+  var temp = from.value;
+  from.value = to.value;
+  to.value = temp;
+}
